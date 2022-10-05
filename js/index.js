a0d54b6195b7817d35e8e3cc3aafa29b98acff3c
just9krish <just9krish434@gamil.com>
2022-10-05 23:44:53 +0530

    working on animation

--- js/index.js
+++ js/index.js
@@ -1,68 +1,63 @@
 const deadLine = "2023-1-1";
 
+updateTimer(getTimeRemaining(deadLine))
+
+let timeinterval = setInterval(() => {
+    updateTimer(getTimeRemaining(deadLine));
+}, 1000);
+
 function getTimeRemaining(endTime) {
     // converting the date into miliseconds so that we can subtract two date like value
     const total = Date.parse(endTime) - Date.parse(new Date());
-
     // now we want to convert miliseconds to seconds, minutes, hours and days
     const seconds = Math.floor((total / 1000) % 60);
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
     return {
         total,
         seconds,
         minutes,
         hours,
-        days
-    }
+        days,
+    };
 }
 
-
-function initializeTimer(endTime) {
-    const days = document.getElementById("timerDays")
-    const hours = document.getElementById("timerHours")
-    const minutes = document.getElementById("timerMinutes")
-    const seconds = document.getElementById("timerSeconds")
-
-
-    function updateTimer() {
-        const t = getTimeRemaining(endTime)
-
-        function a() {
-            let tempsec = t.seconds
-            // console.log(tempsec)
-            // console.log(t.seconds)
-            setTimeout(() => {
-                if (t.seconds != tempsec) {
-                    let secStyle = seconds.style
-                    console.log(secStyle)
-                    
-                }
-            }, 100)
-        }
-
-        days.innerHTML = (`0${t.days}`).slice(-2)
-        hours.innerHTML = (`0${t.hours}`).slice(-2)
-        minutes.innerHTML = (`0${t.minutes}`).slice(-2)
-        seconds.innerHTML = (`0${t.seconds}`).slice(-2)
-
-        if (t.total <= 0) {
-            clearInterval(timeinterval)
-        }
-        a()
-    }
-
-    updateTimer()
-
-    let timeinterval = setInterval(updateTimer, 1000)
+function classRemover(className) {
+    setTimeout(() => {
+        className.classList.remove("flip-animation")
+    }, 800)
 }
 
-// Step that our initializeClock function doing every second
-// 1 = calculate the remaining time
-// 2 = output the remaining time to our p tag
-// 3 = if remaining time gets zero stop the clock
+function updateTimer(data) {
+    const secondsElements = document.getElementById("timerSeconds");
+    const minutesElements = document.getElementById("timerMinutes");
+    const hoursElements = document.getElementById("timerHours");
+    const daysElements = document.getElementById("timerDays");
 
-initializeTimer(deadLine)
+    if (data.seconds != secondsElements.innerHTML) {
+        secondsElements.classList.add("flip-animation")
+        classRemover(secondsElements)
+    }
+    if (data.minutes != minutesElements.innerHTML) {
+        minutesElements.classList.add("flip-animation")
+        classRemover(minutesElements)
+    }
+    if (data.hours != hoursElements.innerHTML) {
+        hoursElements.classList.add("flip-animation")
+        classRemover(hoursElements)
+    }
+    if (data.days != daysElements.innerHTML) {
+        daysElements.classList.add("flip-animation")
+        classRemover(daysElements)
+    }
 
+    daysElements.innerHTML = (`0${data.days}`).slice(-2)
+    hoursElements.innerHTML = (`0${data.hours}`).slice(-2)
+    minutesElements.innerHTML = (`0${data.minutes}`).slice(-2)
+    secondsElements.innerHTML = (`0${data.seconds}`).slice(-2)
+
+    if (data.total <= 0) {
+        clearInterval(timeinterval);
+    }
+}
